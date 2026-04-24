c24ce735c2175983fc7af335d2202a37e442220a
Test MS <z.timur83@gmail.com>
2026-04-24 10:29:28 +0500

    Initial Commit

---
+++ NavigationSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NavigationSample
+//
+//  Created by Timur Zakirov on 24/04/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NavigationSampleTests/NavigationSampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationSampleTests.swift
+//  NavigationSampleTests
+//
+//  Created by Timur Zakirov on 24/04/26.
+//
+
+import Testing
+@testable import NavigationSample
+
+struct NavigationSampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
