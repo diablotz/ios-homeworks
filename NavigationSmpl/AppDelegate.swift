0bef4dd6fabddbd08934309e808d4956fe4bb276
Test MS <z.timur83@gmail.com>
2026-05-06 12:06:30 +0500

    Завершенный проект IOS UI 06052026

--- NavigationSmpl/AppDelegate.swift
+++ NavigationSmpl/AppDelegate.swift
@@ -4,33 +4,67 @@
 //
 //  Created by Timur Zakirov on 06/05/26.
 //
-
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    // В iOS 26 переменная window здесь больше не нужна,
+    // она переехала в SceneDelegate.swift
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // Здесь можно инициализировать сторонние библиотеки (Firebase, SDK аналитики и т.д.)
         return true
     }
 
     // MARK: UISceneSession Lifecycle
+    // Эти методы сообщают приложению, что нужно использовать SceneDelegate
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
+        // Вызывается при создании новой "сцены" (окна)
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+        // Вызывается, когда пользователь закрывает сцену (например, смахивает окно в многозадачности)
     }
+}
+/*
+import UIKit
 
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        // create tab bar with feed and profile items
+        let loginVC = LoginViewController()
+        let profileNC = UINavigationController(rootViewController: loginVC)
+        profileNC.tabBarItem = UITabBarItem(title: "Profile",
+                                            image: UIImage(systemName: "person.crop.circle"),
+                                            selectedImage: UIImage(systemName: "person.crop.circle.fill"))
+        
+        let feedVC = FeedViewController()
+        let feedNC = UINavigationController(rootViewController: feedVC)
+        feedNC.tabBarItem = UITabBarItem(title: "Feed",
+                                         image: UIImage(systemName: "text.bubble"),
+                                         selectedImage: UIImage(systemName: "text.bubble.fill"))
+
+        let tabBarController = UITabBarController()
+        tabBarController.tabBar.backgroundColor = .white
+        tabBarController.viewControllers = [profileNC, feedNC]
+        
+        // activate main window
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = tabBarController
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
 }
 
+*/
