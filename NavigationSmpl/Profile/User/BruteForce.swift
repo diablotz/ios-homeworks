7d8c08ac72131cb280d8030268286b2020c6f591
Test MS <z.timur83@gmail.com>
2026-05-29 13:42:21 +0500

    Промышленная разработка. ДЗ№10 29052026

---
+++ NavigationSmpl/Profile/User/BruteForce.swift
@@ -0,0 +1,58 @@
+//
+//  BruteForce.swift
+//  NavigationSmpl
+//
+//  Created by Timur Zakirov on 28/05/26.
+//
+
+import UIKit
+
+final class BruteForce {
+    // массив символов для генерации пароля
+    private let symbols = Array("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+    
+    // генерация пароля
+    func generatePassword(length: Int) -> String {
+        var password = ""
+        for _ in 0..<length {
+            let randomIndex = Int.random(in: 0..<symbols.count)
+            password.append(symbols[randomIndex])
+        }
+        print ("password: \(password)")
+        return password
+    }
+    
+    
+    var attempts: Int = 0 // для счетчика кол-ва операций
+    
+    // подборка пароля
+    func bruteForce(password: String) -> String? {
+        
+        let targetLength = password.count
+        
+        func generate(current: String) -> String? {
+            
+            if current.count == targetLength {
+                
+                attempts += 1
+                
+                if current == password {
+                    print ("Найден пароль = \(current) за перебор вариантов =  \(attempts)")
+                    return current
+                }
+                return nil
+            }
+            
+            for symbol in symbols {
+                if let result = generate(current: current + String(symbol)) {
+                    return result
+                }
+            }
+            
+            return nil
+        }
+        
+        return generate(current: "")
+    }
+    
+}
