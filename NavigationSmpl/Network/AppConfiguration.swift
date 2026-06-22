47f05f1ea41baf075a3ee9e6ad0b34606b09c6e3
Test MS <z.timur83@gmail.com>
2026-06-22 18:31:53 +0500

    Хранение данных. ДЗ№1 22062026

---
+++ NavigationSmpl/Network/AppConfiguration.swift
@@ -0,0 +1,14 @@
+//
+//  AppConfiguration.swift
+//  NavigationSmpl
+//
+//  Created by Timur Zakirov on 22/06/26.
+//
+
+import Foundation
+
+enum AppConfiguration {
+    case people(URL)
+    case starships(URL)
+    case planets(URL)
+}
