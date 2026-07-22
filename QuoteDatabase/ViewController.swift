68cf8fe10ea31c9bd00847e05b1f5e40631e70c0
Test MS <z.timur83@gmail.com>
2026-07-22 10:51:03 +0500

    Initial Commit

---
+++ QuoteDatabase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuoteDatabase
+//
+//  Created by Timur Zakirov on 22/07/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ QuoteDatabaseTests/QuoteDatabaseTests.swift
@@ -0,0 +1,19 @@
+//
+//  QuoteDatabaseTests.swift
+//  QuoteDatabaseTests
+//
+//  Created by Timur Zakirov on 22/07/26.
+//
+
+import Testing
+@testable import QuoteDatabase
+
+struct QuoteDatabaseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
